d5e0fd2b9c6492d7fdbcc199367823cc70f6a5a5
Zenrool <lorenzopapperini@hotmail.it>
2022-04-22 16:02:11 +0200

    Struttura appare la griglia

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-const btnClick = () =>
+// MAIN Function
+const btnClick = () => {
+    const gridSize = 100;
+    const gridArray = [];
+    for (let i = 0 ; i < gridSize ; i++) {
+        gridArray.push(i+1);
+        console.log(1);
+    }
+    console.log(gridArray);
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < gridArray.length; i++) {
+        const thisNumber = gridArray[i];
+
+        const domElement = generateGridItem(thisNumber)
+
+        domElement.addEventListener("click", function() {
+            this.classList.add("active");
+        });
+
+        gridContainer.append(domElement);
+        gridContainer.classList.remove("none");
+    }    
+
+
+}
+
+function generateGridItem(number) {
+    const newElement = document.createElement("div");
+
+    newElement.classList.add("grid-item");
+    
+    newElement.innerHTML = `<span>${number}</span>`
+
+    return newElement;
+}
